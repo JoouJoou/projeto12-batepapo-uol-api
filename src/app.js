3d2932e0ea8fd77d8bdbc420559de8c8b77a3fe1
JouJou <joujou@pop-os.localdomain>
2022-09-02 20:06:21 -0300

    Route post participants

--- src/app.js
+++ src/app.js
@@ -15,16 +15,13 @@ app.use(cors());
 app.use(express.json());
 
 const participantSchema = joi.object({
-    name: joi.string().empty().required(),
-    lastStatus: joi.number().min(1).required()
+    name: joi.string().required()
 })
 
 const messageSchema = joi.object({
-    from: joi.string().empty().required(),
-    to: joi.string().empty().required(),
-    text: joi.string.empty().required(),
+    to: joi.string().required(),
+    text: joi.string().required(),
     type: joi.string().valid("private_message", "message").required(),
-    time: joi.string().required()
 })
 
 const mongoClient = new MongoClient(process.env.MONGO_URI);
@@ -33,5 +30,25 @@ mongoClient.connect().then(() => {
     db = mongoClient.db("batepapouol")
 })
 
+app.post("/participants", async (req, res) => {
+    try {
+        const validation = participantSchema.validate(req.body, { abortEarly: true })
+        if (validation.error) {
+            res.status(422).send("Nomes devem ser strings não vazias")
+            return
+        }
+        const checkName = await db.collection("participants").findOne(req.body)
+        if (checkName) {
+            res.status(409).send("Usuário já cadastrado")
+            return
+        }
+        await db.collection("participants").insertOne({name: req.body.name, lastStatus: Date.now()})
+        res.sendStatus(201)
+        return
+    } catch {
+        res.sendStatus(500)
+        return
+    }
+})
 
 app.listen(5000, () => {console.log("Listen on 5000")});
